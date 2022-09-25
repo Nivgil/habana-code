97ddc30abb7d276cd5243fdc14f100e686ca2003
nivgil <giladiniv@gmail.com>
2022-09-26 00:42:49 +0300

    DEBUG: raise error in hooks in case of compute time violation

--- PyTorch/nlp/pretraining/bert/run_pretraining.py
+++ PyTorch/nlp/pretraining/bert/run_pretraining.py
@@ -777,7 +777,6 @@ def main():
                         time.time() - compute_logs['start_compute'])
                 if compute_logs['enable_drop'] and (
                         current_time_passed >= compute_logs['threshold']):
-                    print(f'Rank {torch.distributed.get_rank()} DROP after {current_time_passed:3.4}')
                     raise ComputeTimeout(module_name)
             return log_time
 
